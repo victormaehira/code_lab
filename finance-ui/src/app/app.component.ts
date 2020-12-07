60ae7333fc488ba155f54e7e34b805332157f548
Victor Maehira <vymaehira@gmail.com>
2020-12-07 01:17:58 -0300

    Reference variable added.

--- finance-ui/src/app/app.component.ts
+++ finance-ui/src/app/app.component.ts
@@ -8,16 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = 'Thiago';
 
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`);
-
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'João ' + numero;
-  }
-
-  alterarNome(event: any) {
-    // console.log(event);
-    this.nome = event.target.value;
+  adicionar(nome: string) {
+    this.nome = nome;
   }
 
 }
