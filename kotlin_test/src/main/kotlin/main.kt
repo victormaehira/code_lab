3c3cfcb9cb218b2020f898e43979878f7df0e74e
Victor Maehira <vymaehira@gmail.com>
2021-02-07 22:04:13 -0300

    Kotlin test added

--- kotlin_test/src/main/kotlin/main.kt
+++ kotlin_test/src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    var x = 1
+    var y = 2
+
+    //constante
+    val z = 3
+
+    var soma = x + y + z
+    println("soma = " + soma)
 }
