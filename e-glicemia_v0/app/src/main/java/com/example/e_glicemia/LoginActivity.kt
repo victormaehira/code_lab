d33eed7f499e32c65abceb2eca1ea7e894ea68ad
Victor Maehira <vymaehira@gmail.com>
2021-03-06 22:10:17 -0300

    Login activity added.

--- e-glicemia_v0/app/src/main/java/com/example/e_glicemia/LoginActivity.kt
+++ e-glicemia_v0/app/src/main/java/com/example/e_glicemia/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.example.e_glicemia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonLoginEntrar.setOnClickListener {
+            val email = editTextLoginEmailAddress.text.toString()
+            val password = editTextLoginPassword.text.toString()
+
+            Toast.makeText(this, "Login Tapped", Toast.LENGTH_SHORT).show()
+        }
     }
 }
