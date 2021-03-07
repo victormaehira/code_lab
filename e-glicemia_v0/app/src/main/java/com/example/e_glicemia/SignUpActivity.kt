2d056de6e904841206da18e2e544139510f412ad
Victor Maehira <vymaehira@gmail.com>
2021-03-06 23:31:51 -0300

    Save button added to SignUp Activity.

--- e-glicemia_v0/app/src/main/java/com/example/e_glicemia/SignUpActivity.kt
+++ e-glicemia_v0/app/src/main/java/com/example/e_glicemia/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.example.e_glicemia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -13,6 +14,14 @@ class SignUpActivity : AppCompatActivity() {
 
         buttonSignUpSair.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        buttonSignUpSalvar.setOnClickListener {
+            val nome = editTextSignUpPersonName.text.toString()
+            val email = editTextSignUpEmailAddress.text.toString()
+            val password = editTextSignUpPassword.text.toString()
+
+            Toast.makeText(this, "Login Tapped", Toast.LENGTH_SHORT).show()
         }
 
     }
