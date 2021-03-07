b0cdd084d2883869af7a185f240bd23d9d298d72
Victor Maehira <vymaehira@gmail.com>
2021-03-06 22:46:19 -0300

    SignUp activity buttons added.

--- e-glicemia_v0/app/src/main/java/com/example/e_glicemia/SignUpActivity.kt
+++ e-glicemia_v0/app/src/main/java/com/example/e_glicemia/SignUpActivity.kt
@@ -1,11 +1,19 @@
 package com.example.e_glicemia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        buttonSignUpSair.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
     }
 }
