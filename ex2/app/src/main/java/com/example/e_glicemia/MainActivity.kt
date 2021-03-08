0b65fbe8c6ad3b51ec045e8ae354b005414a6d4f
Victor Maehira <vymaehira@gmail.com>
2021-03-08 00:06:03 -0300

    Adicionada Glicemia na app.

--- ex2/app/src/main/java/com/example/e_glicemia/MainActivity.kt
+++ ex2/app/src/main/java/com/example/e_glicemia/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.e_glicemia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import com.example.e_glicemia.model.Glicemia
 
 class MainActivity : AppCompatActivity() {
+    val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +19,25 @@ class MainActivity : AppCompatActivity() {
 
         textViewMainName.text = "Olá, $userName"
 
+        buttonMainAdicionar.setOnClickListener {
+            val valorDoFront = 123
+
+            adicionar(valorDoFront)
+        }
+
     }
+
+    private fun adicionar(valorDoFront: Int) {
+        val currentUser = Firebase.auth.currentUser!!
+
+        val glicemia = Glicemia(
+            userId = currentUser.uid,
+            userName = currentUser.displayName,
+            valor = valorDoFront
+        )
+
+        db.collection("glicemias")
+            .add(glicemia)
+    }
+
 }
---
+++ ex2/app/src/main/java/com/example/e_glicemia/model/Glicemia.kt
@@ -0,0 +1,7 @@
+package com.example.e_glicemia.model
+
+class Glicemia (
+    val userId: String,
+    val userName: String?,
+    val valor: Int
+)
