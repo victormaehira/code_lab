7239fbbe7a4572a4899c45d4ecdb7cfbaf3d1cc2
victor maehira <vymaehira@gmail.com>
2021-08-02 19:05:26 -0300

    kafka producer backup added.

---
+++ bytebank/src/app/deleteme/deleteme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deleteme',
+  templateUrl: './deleteme.component.html',
+  styleUrls: ['./deleteme.component.scss']
+})
+export class DeletemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
